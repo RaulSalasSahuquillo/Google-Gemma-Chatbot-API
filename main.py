04acab56fbbf0805972eb6aa4837361fab5c0e09
Raúl Salas Sahquillo <salassahuquilloraul@gmail.com>
2025-12-25 23:34:26 +0100

    Chat loop added + memory so that you can have an actual conversation with the chatbot.

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ import os
 
 load_dotenv() # Carga las variables de entorno desde el archivo .env
 
+# Introduce la IA en la terminal
 print("EN.AI, powered by Gemini, developed by Raúl Salas Sahuquillo")
 
 # Inicializa el cliente con la clave API
 api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
+    print("ERROR: La clave API no es válida o no está configurada. Por favor, consisidera crear un archivo .env con GEMINI_API_KEY=tu_clave_aquí.")
+    exit()
 client = genai.Client(api_key=api_key) # Se usa os para ocultar la clave API
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Hola" # Model para modelo de Gemini y el prompt es contents
+chat = client.chats.create(
+    model = "gemini-2.5-flash", # Modelo de Gemini más avanzado disponible como API
+    config = {
+        "system_instruction": "Eres EN.AI, una IA desarrollada por el estudio personal de ENEI PROJECT que pertenece al desarrollador Raúl Salas Sahuquillo. Responde de manera útil, amigable y profesional. Habla en español.",
+    }
 )
-print(response.text)
+
+while True:
+    user_input = input("Escribe tu pregunta: ") # Entrada del usuario
+    if user_input.lower().strip() in ["exit", "adiós", "salir", "adios", "bye"]:
+        print("Fin del chat.")
+        break
+    if not user_input:
+        continue
+    try:
+        response = chat.send_message(user_input)
+        print("EN.AI:", response.text) # Muestra la respuesta de la IA
+    except Exception as e:
+        print(f"Ocurrió un error al comunicarse con la API de Gemini: {e}")
