e93dc732ac9d4bf0d8dbaa8e05ca23b68ef00edc
Raúl Salas Sahquillo <salassahuquilloraul@gmail.com>
2025-12-26 13:01:42 +0100

    Ahora la IA se connecta a través de internet para tener información actual

--- app.py
+++ app.py
@@ -14,20 +14,25 @@ if not api_key:
     exit()
 client = genai.Client(api_key=api_key) # Se usa os para ocultar la clave API
 
+# Definimos el modelo de Gemini y su base de configuración al responder
 chat = client.chats.create(
     model = "gemini-2.5-flash", # Modelo de Gemini más avanzado disponible como API
     config = {
         "system_instruction": "Eres EN.AI, una IA desarrollada por el estudio personal de ENEI PROJECT que pertenece al desarrollador Raúl Salas Sahuquillo. Responde de manera útil, amigable y profesional. Habla en español.",
+        "tools": [{"google_search": {}}], # Habilita la herramienta de búsqueda en Google
     }
 )
 
+# Bucle principal para interactuar con la IA
 while True:
     user_input = input("Escribe tu pregunta: ") # Entrada del usuario
+    # Condición para salir del chat
     if user_input.lower().strip() in ["exit", "adiós", "salir", "adios", "bye"]:
         print("Fin del chat.")
         break
     if not user_input:
         continue
+    # Envía el mensaje del usuario a la IA y obtiene la respuesta
     try:
         response = chat.send_message(user_input)
         print("EN.AI:", response.text) # Muestra la respuesta de la IA
